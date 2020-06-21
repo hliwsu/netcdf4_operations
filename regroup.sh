72ef9df8e19fa33b16e819c8db42c14086001a0c
hliwsu <51107130+hliwsu@users.noreply.github.com>
2020-06-21 01:08:56 -0700

    historical and future values reassigned

--- regroup.sh
+++ regroup.sh
@@ -5,5 +5,12 @@ do
     ncap2 -s  'where(VTYPE == 4 || 5 || 6 || 7 || 8 || 9 || 10 || 11 || 12 || 13 || 14 || 15 || 16 || 21 || 22 || 25 || 26 || 27 || 36 ) VTYPE=1' $file -O $file
     ncap2 -s  'where(VTYPE == 2 || 28 ) VTYPE=0' $file -O $file
  done
+cd /mnt/d/temp/vtype/hist
+for file in  *.nc
+do 
+    ncap2 -s  'where(VTYPE == 4 || 5 || 6 || 7 || 8 || 9 || 10 || 11 || 12 || 13 || 14 || 15 || 16 || 21 || 22 || 25 || 26 || 27 || 36 ) VTYPE=1' $file -O $file
+    ncap2 -s  'where(VTYPE == 2 || 28 ) VTYPE=0' $file -O $file
+ done
+
 
 
